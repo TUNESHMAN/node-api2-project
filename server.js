100bf3e598fea1dcbcfe37a45c522fc9b53ee7fb
Babatunde <babatundea15@gmail.com>
2020-05-11 15:41:08 +0100

    I plugged extra functionalities

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const cors = require("cors");
 const port = 5000;
 // Flesh out a dummy server
 const server = express();
+// Plug extra functionality to the server
+server.use(express.json());
+
+// I enabled CORS so that the server works for all origins
+server.use(cors());
 
 server.get("/", (req, res) => {
   res.send("Hello from server");
